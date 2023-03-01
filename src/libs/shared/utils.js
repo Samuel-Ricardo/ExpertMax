799dfc584c8881bd3fd5058d15f5a2a8f1c57ca4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-01 19:06:39 -0300

    :sparkles: create: util - supportWorkerType

---
+++ src/libs/shared/utils.js
@@ -0,0 +1,11 @@
+function supportWorkerType() {
+    let supports = false
+    const tester = {
+        get type() { supports = true }
+    }
+
+    try { new Worker('blob://', tester).terminate() }
+    finally{ return supports }
+}
+
+export {supportWorkerType};
