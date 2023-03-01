96fdd5e5689d54d8a9e503db7f6813fb33de7df6
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-01 19:15:30 -0300

    :sparkles: update: view - video-player

--- src/pages/video-player/src/view.js
+++ src/pages/video-player/src/view.js
@@ -1,3 +1,14 @@
 export default class View {
+
+    #btnInit = document.querySelector("#init");
+    #statusElement = document.querySelector("#status");
+
     constructor({}) {}
+
+    enableButton() { this.#btnInit.disabled = false; }
+
+    configureOnBtnClick(fun) { this.#btnInit.addEventListener('click', fun) }
+
+    log(text) { this.#statusElement.innerHTML = text }
+
 }
