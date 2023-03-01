ec8a5fe444507fdd0572b40eaa6f8b5f7e77afb4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-01 19:11:46 -0300

    :sparkles: handle: browser support - worker

--- src/pages/video-player/src/factory.js
+++ src/pages/video-player/src/factory.js
@@ -1,14 +1,33 @@
 import Camera from "../../../libs/shared/cam.js";
+import { supportWorkerType } from "../../../libs/shared/utils.js";
 import Controller from "./controller.js";
 import Service from "./service.js";
 import View from "./view.js";
 
+async function getWorker(){
+    if (supportWorkerType()) {  console.log("SUPORT")
+    
+        const worker = new Worker('./src/worker.js', {type: 'module'});
+        return worker;
+    }
+
+
+    else { console.log("NOT SUPORT")
+
+        const workerMock = {
+            async postMessage(){},
+            onmessage(msg) {}
+        }
+        return workerMock
+    }
+}
+
+const worker = await getWorker();
+
+const camera = await Camera.init();
 const [rootPath] = window.location.href.split( '/pages/' );
 const factory = {
     async initialize() {
-        
-        const camera = await Camera.init();
-
         return Controller.initialize({
             view: new View({}),
             service: new Service({}),
