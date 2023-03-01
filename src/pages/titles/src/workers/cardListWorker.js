75bf6bd47950e27627ef0dadeff4238461519cc2
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-02-28 23:30:49 -0300

    :zap: create: worker - Card List

---
+++ src/pages/titles/src/workers/cardListWorker.js
@@ -0,0 +1,18 @@
+onmessage = ({ data }) => {
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    console.log(data);
+
+    let counter = 0;
+    
+
+        console.log('activating blocking operation...', data.maxItems)
+        console.time('blocking-op')
+        // blocking function
+        // 1e5 = 100.000
+        for (counter; counter < data.maxItems; counter++) console.log('.')
+        console.timeEnd('blocking-op')
+
+
+
+    postMessage({response: "ok :D", data: counter})
+}
