7e02d33c12e1d564fdef2971dc77497da79db993
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-01 18:29:58 -0300

    :camera_flash: handle: webcam

---
+++ src/libs/shared/cam.js
@@ -0,0 +1,41 @@
+export default class Camera {
+    constructor() {
+        this.video = document.createElement('video');
+    }
+
+    static async init() {
+        if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia){
+            throw new Error("Your browser does not support getUserMedia API");
+        }
+
+        const videoConfig = {
+            audio: false,
+            video: {
+                width: globalThis.screen.availWidth,
+                height: globalThis.screen.availHeight,
+                frameRate: { ideal: 60 }
+            }
+        }
+
+        const stream = await navigator.mediaDevices.getUserMedia(videoConfig);
+        const camera = new Camera();
+
+        camera.video.srcObject = stream; 
+
+        camera.video.height = 240;
+        camera.video.width = 320;
+
+        document.body.append(camera.video)
+
+        // aguardar pela camera!
+        await new Promise((resolve) => {
+            camera.video.onloadedmetadata = () => {
+                resolve(camera.video);
+            }
+        })
+
+        camera.video.play();
+
+        return camera
+    }
+}
