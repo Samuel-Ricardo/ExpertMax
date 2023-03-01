a6ccc67e8c283dc664143ee3b55ee70858612766
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-01 19:18:59 -0300

    :sparkles: log: controller - init

--- src/pages/video-player/src/controller.js
+++ src/pages/video-player/src/controller.js
@@ -9,10 +9,13 @@ export default class Controller {
 
     static async initialize(deps) {
         const controller = new Controller(deps);
+        controller.log('not yet detecting eye blink! click in the button to start')
         return controller.init();
     }
 
     async init() { console.log("init!!") }
 
     log(text) { this.#view.log(`Logger: ${text}`) }
+
+    
 }
