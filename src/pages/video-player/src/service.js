324331b4f32689f9852159f8e14c4aae461447f9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-02 10:30:02 -0300

    :sparkles: create: fun - getEAR (service)

--- src/pages/video-player/src/service.js
+++ src/pages/video-player/src/service.js
@@ -14,6 +14,27 @@ export default class Service {
         )
     }
 
+
+      // Calculate the position of eyelid to predict a blink
+    #getEAR(upper, lower) {
+        function getEucledianDistance(x1, y1, x2, y2) {
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+        }
+
+        return (
+        (getEucledianDistance(upper[5][0], upper[5][1], lower[4][0], lower[4][1])
+            + getEucledianDistance(
+            upper[3][0],
+            upper[3][1],
+            lower[2][0],
+            lower[2][1],
+            ))
+        / (2
+            * getEucledianDistance(upper[0][0], upper[0][1], upper[8][0], upper[8][1]))
+        )
+    }
+
+
     async handBlinked(video) {
         const predictions = await this.#estimateFaces(video)
         console.log({predications: predictions})
