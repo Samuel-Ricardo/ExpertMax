212dd1322581e1e7584775762fd96660a5f343fa
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-02 11:10:38 -0300

    :recycle:

--- src/pages/video-player/src/factory.js
+++ src/pages/video-player/src/factory.js
@@ -15,10 +15,10 @@ async function getWorker(){
     console.warn("Your Browser doesn't support esm modules on webworkers!")
     console.warn("Importing libraries...")
 
-    await import "https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js"
-    await import "https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js"
-    await import "https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js"
-    await import "https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js"
+   // await import "https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js"
+   // await import "https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js"
+   // await import "https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js"
+   // await import "https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js"
 
     console.warn(`using worker mock instead!`)
     const service = new Service({faceLandmarksDetection: window.faceLandmarksDetection})
