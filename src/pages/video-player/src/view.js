338c1911d7fe1278e45b96d37d28ddb9d3979cfc
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-01 21:57:07 -0300

    :sparkles: create: function - getFrameVideo (view)

--- src/pages/video-player/src/view.js
+++ src/pages/video-player/src/view.js
@@ -2,8 +2,20 @@ export default class View {
 
     #btnInit = document.querySelector("#init");
     #statusElement = document.querySelector("#status");
+    #videoFrameCanvas = document.createElement("canvas")
+    #canvasContext = this.#videoFrameCanvas.getContext("2d", {willReadFrequently: true});
 
-    constructor({}) {}
+
+    getVideoFrame(video) {
+        const canvas = this.#videoFrameCanvas
+        const [width,heigth] = [video.videoWidth,video.videoHeight];
+        
+        canvas.width = width
+        canvas.height = height
+
+        this.#canvasContext.drawImage(video, 0, 0, width, height)
+        return this.#canvasContext.getImageData(0,0,width,heigth)
+    }
 
     enableButton() { this.#btnInit.disabled = false; }
 
