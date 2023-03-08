1494b8abae88f01c2d8b73c689f6ff8efeec1a7a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-08 10:45:56 -0300

    :sparkles: create: fun - initialize (HandGestureController)

--- src/pages/titles/src/controllers/handGestureController.js
+++ src/pages/titles/src/controllers/handGestureController.js
@@ -1,11 +1,28 @@
 export default class HandGestureController {
   #view
   #service
-  constructor(view, service) {
+  #camera
+  constructor({view, service, camera}) {
     this.#view = view;
     this.#service = service;
+
+    this.#camera = camera
   }
-    
+
+  async init() {
+    return this.#loop()
+  }
+
+  async #estimateHands() {
+    try{ 
+      const hands = await this.#service.estimateHands(this.#camera.video);
+      console.log({hands}) 
+     }
+
+     catch(error) { console.log("Deu ruim :/", {error}) }
+  }
+
+
   static async initialize(deps){
     const controller = new HandGestureController(deps);
     return controller.init()
