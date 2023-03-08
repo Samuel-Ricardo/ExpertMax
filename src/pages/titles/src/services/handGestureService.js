681fabf4b5940b8800a00812df3862b15f4bfbd5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-08 10:25:34 -0300

    :sparkles: create: function - initializeDetector (HandGestureService)

--- src/pages/titles/src/services/handGestureService.js
+++ src/pages/titles/src/services/handGestureService.js
@@ -2,9 +2,30 @@ export default class HandGestureService {
   #fingerpose
   #handPoseDetection
   #handsVersion
+  #detector = null
+
   constructor({ fingerpose, handPoseDetection, handsVersion }){
     this.#fingerpose = fingerpose 
     this.#handPoseDetection = handPoseDetection 
     this.#handsVersion = handsVersion
+  }
+
+  async initializeDetector() {
+    if (this.#detector) return this.#detector;
+
+    const detectorConfig = {
+      runtime: 'mediapipe', // or tfjs
+      solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${this.#handsVersion}`,
+      // full - Heavier and more accurate
+      modelType: 'lite',
+      maxHands: 2
     }
+
+    this.#detector = await this.#handPoseDetection.createDetector(
+      this.#handPoseDetection.SupportedModels.MediaPipeHands,
+      detectorConfig
+    );
+
+    return this.#detector;
+  }
 }
