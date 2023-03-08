325689d8eb1c93aaf8dd9ebb478d71649c2a2654
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-08 11:03:53 -0300

    :sparkles: identify - close hand (Rock)

--- src/pages/titles/src/utils/gestures.js
+++ src/pages/titles/src/utils/gestures.js
@@ -4,5 +4,12 @@ const RockGesture = new GestureDescription('rock'); // ✊️
 const PaperGesture = new GestureDescription('paper'); // 🖐
 
 
+RockGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1.0);
+RockGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 0.5);
+
+for(let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
+    RockGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
+    RockGesture.addCurl(finger, FingerCurl.NoCurl, 0.9);
+}
 
 
