3d24b7c34172d660747764b930b1f866a4e8862d
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-08 13:17:00 -0300

    :sparkles: create: fun - scrollPage (HandGestureView)

--- src/pages/titles/src/views/handGestureView.js
+++ src/pages/titles/src/views/handGestureView.js
@@ -1,3 +1,10 @@
 export default class HandGestureView {
     loop(fun) { requestAnimationFrame(fun) } //60fps
+
+    scrollPage(top){
+        scroll({
+            top,
+            behavior: 'smooth'
+        })
+    }
  }
