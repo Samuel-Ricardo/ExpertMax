a26badd0af0eaa1cd6d762fd2dcd0796f2b83414
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-09 09:42:28 -0300

    :sparkles: create: fun - #drawnFingersAndHoverElements (HandGestureView)

--- src/pages/titles/src/views/handGestureView.js
+++ src/pages/titles/src/views/handGestureView.js
@@ -2,10 +2,13 @@ export default class HandGestureView {
 
     #handsCanvas = document.querySelector('#hands')
     #canvasContext = this.#handsCanvas.getContext('2d')
+    #fingerLookupIndexes
 
-    constructor() {
+    constructor({fingerLookupIndexes}) {
         this.#handsCanvas.width = globalThis.screen.availWidth
         this.#handsCanvas.height = globalThis.screen.availHeight
+
+        this.#fingerLookupIndexes = fingerLookupIndexes
     }
 
     clearCanvas() {
@@ -16,13 +19,14 @@ export default class HandGestureView {
         for (const {keypoints, handedness} of hands) {
             if(!keypoints) continue
 
-            this.#canvasContext.fillStyle = handedness === "Left"? "red" : "green"
-            this.#canvasContext.strokeStyle = "white"
+            this.#canvasContext.fillStyle = handedness === "Left"? "red" : "rgb(44, 212, 103)"
+            this.#canvasContext.strokeStyle = "black"
             this.#canvasContext.lineWidth = 8
             this.#canvasContext.lineJoin = "round"
 
             //juntas dos dedos
             this.#drawnJoients(keypoints)
+            this.#drawnFingersAndHoverElements(keypoints)
         }
     }
 
@@ -30,14 +34,31 @@ export default class HandGestureView {
         for (const {x, y} of keypoints){
             this.#canvasContext.beginPath()
 
-            const newX = x - 2;
-            const newY = y - 2;
-            const radius = 3;
+            const newX = x - 1;
+            const newY = y - 1;
+            const radius = 5;
             const startAngle = 0;
             const endAngle = 2*Math.PI;
 
             this.#canvasContext.arc(newX, newY, radius, startAngle, endAngle)
             this.#canvasContext.fill()
+        }
+    }
+
+    #drawnFingersAndHoverElements(keypoints){
+        const fingers = Object.keys(this.#fingerLookupIndexes) 
+
+        for (const finger of fingers) {
+            const points = this.#fingerLookupIndexes[finger].map(index => keypoints[index])
+
+            const region = new Path2D()
+            // [0] hand palm
+            const [{x, y}] = points
+            region.moveTo(x,y)
+
+            for(const point of points) region.lineTo(point.x, point.y)
+
+            this.#canvasContext.stroke(region)
         }
     }
 
